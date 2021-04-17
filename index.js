bf3f6b87f1689154ca07b4c37945fcaddf940ebc
RomainMefret <romain.mefret@healthygaming.fr>
2021-04-17 16:31:52 +0200

    connection à mongoDB OK

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
-  console.log("Backend server is running!");
+  console.log("Backend server is running on 8800");
 });
--- routes/auth.js
+++ routes/auth.js
@@ -5,15 +5,15 @@ const bcrypt = require("bcrypt");
 //REGISTER
 router.post("/register", async (req, res) => {
   try {
-    //generate new password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    // //generate new password
+    // const salt = await bcrypt.genSalt(10);
+    // const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
     //create new user
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: hashedPassword,
+      password: req.body.password,
     });
 
     //save user and respond
