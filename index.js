fe432f81bca368576236c856b1a2416876792f80
RomainMefret <romain.mefret@healthygaming.fr>
2021-04-20 22:00:49 +0200

    debug

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const cookieParser = require("cookie-parser");
 const { userCheck, requireAuth } = require("./middleware/auth.middleware");
+const cors = require("cors");
 
 require("dotenv").config({ path: "./config/.env" });
 
 require("./config/db");
 
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
@@ -28,6 +30,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(8800, () => {
-  console.log("Backend server is running on 8800");
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Backend server is running on port : ${port}`);
 });
